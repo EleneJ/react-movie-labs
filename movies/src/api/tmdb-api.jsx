6c63f68c7fc45dc2ceb4069dca0072068466b520
Elene <20101559@mail.wit.ie>
2025-03-26 16:14:58 +0000

    Added Upcoming movies page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -86,3 +86,13 @@ export const getMovie = (args) => {
       throw error
    });
   };
+
+  export const getUpcomingMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
+    );
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    return response.json();
+  };
